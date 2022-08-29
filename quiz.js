ee37b4cbb32fa0f7bee3d855f11bab12b2ee199f
jocelyn valladares <jocelynvalladares933@gmail.com>
2022-08-29 11:40:19 -0700

    text

--- quiz.js
+++ quiz.js
@@ -9,4 +9,59 @@ const choiceC = document.getElementById("C");
 const counter = document.getElementById("counter");
 const timeGauge = document.getElementById("timeGauge");
 const progress = document.getElementById("progress");
-const score = document.getElementById("score");
+const scoreDiv = document.getElementById("score");
+
+// create the questions
+let questions = [ ... ];
+
+// create some variables
+
+const lastQuestion = questions.length - 1;
+let runningQuestion = 0;
+
+// render a question 
+function runningQuestion(){
+    let q = questions[runningQuestion];
+
+    question.innerHTML = "<p>"+ q.question +"</p";
+    qImg.innerHTML = "<img src="+ q.imgSrc +">";
+    choiceA.innerHTML = q.choiceA;
+    choiceB.innerHTML = q.choiceB;
+    choiceC.innerHTML = q.choiceC
+}
+start.style.display = "none";
+renderQuestion();
+quiz.style.display = "block";
+
+// render progress
+function renderProgress(){
+    for(let qIndex = 0; qIndex <= lastQuestion;
+    qIndex++){
+        progress.innerHTML += "<div class='prog'";
+    }
+}
+
+    {
+        question : "what does HTML Stand for?"
+        imgSrc : "img/html.png",
+        choiceA : "correct",
+        choiceB : "wrong",
+        choiceC : "wrong",
+        correct : "A",
+    },{
+        question : "what does css stand for?",
+        imgSrc : "img/",
+        choiceA : "wrong",
+        choiceB : "correct",
+        choiceC : "wrong",
+        correct : "A",
+    },{
+        question : "what does js stand for?",
+        imgSrc : "img/",
+        choiceA : "wrong",
+        choiceB : "wrong",
+        choiceC : "correct",
+        correct : "C",
+
+    }
+]
